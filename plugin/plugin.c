a259b96706378f6f4f9b1e6e6eb9a135aaf71bfb
Noah Johnson <noah.johnson@loupmail.com>
2023-02-25 18:31:29 -0600

    Create plugin.c

---
+++ plugin/plugin.c
@@ -0,0 +1,19 @@
+/*
+ * plugin.c
+ * ========
+ * 
+ * Implementation of plugin.h
+ * 
+ * See the header for further information.
+ */
+
+#include "plugin.h"
+
+/* @@TODO: Import plug-in module headers here */
+
+/*
+ * The plug-in initialization function implementation.
+ */
+void plugin_init(void) {
+  /* @@TODO: Insert calls to plug-in module registrations here */
+}
