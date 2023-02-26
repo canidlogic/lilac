c611cb1863125821abed6d111b7db4b054e2ba53
Noah Johnson <noah.johnson@loupmail.com>
2023-02-25 18:31:10 -0600

    Create plugin.h

---
+++ plugin/plugin.h
@@ -0,0 +1,17 @@
+#ifndef PLUGIN_H_INCLUDED
+#define PLUGIN_H_INCLUDED
+
+/*
+ * plugin.h
+ * ========
+ * 
+ * Plug-in registry for Lilac.  See the README in this directory for
+ * further information.
+ */
+
+/*
+ * The plug-in initialization function.
+ */
+void plugin_init(void);
+
+#endif
