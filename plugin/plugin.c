6dd78952332b7f5b01961156d519ed49a3b01ef6
Noah Johnson <noah.johnson@loupmail.com>
2023-02-25 19:03:47 -0600

    Update plugin.c

--- plugin/plugin.c
+++ plugin/plugin.c
@@ -10,10 +10,12 @@
 #include "plugin.h"
 
 /* @@TODO: Import plug-in module headers here */
+#include "node_constant.h"
 
 /*
  * The plug-in initialization function implementation.
  */
 void plugin_init(void) {
   /* @@TODO: Insert calls to plug-in module registrations here */
+  node_constant_init();
 }
